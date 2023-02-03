13bd549fc0b6c4dd19205d6d8a4f564cc862d29c
gitlG <1637885653@qq.com>
2023-02-04 07:47:51 +0800

    最后更新

--- src/ShowLog.py
+++ src/ShowLog.py
@@ -1,6 +1,6 @@
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import Qt,QPoint,pyqtSlot,QSize
-from  PyQt5.QtWidgets import QTableWidget,QTableWidgetItem,QVBoxLayout,QMenu,QHeaderView,QMessageBox, QDialog
+from  PyQt5.QtWidgets import QTableWidget,QTableWidgetItem,QVBoxLayout,QMenu,QHeaderView,QMessageBox, QDialog,QCheckBox
 from PyQt5 import QtWidgets
 from .ImageView import ShowImage
 from .Database import database
@@ -29,9 +29,7 @@ class ShowLog(QDialog):
         self.tableWidget.setContextMenuPolicy(Qt.CustomContextMenu)#允许右键显示上菜单
         self.tableWidget.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)#禁止用户编辑单元格
         self.tableWidget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)#表示均匀拉直表头
-        # self.qlineedit = QLineEdit()
-        # self.qlineedit.setPlaceholderText('Please enter your usernumber')
-        # self
+        
         self.tableWidget.customContextMenuRequested[QPoint].connect(self.contextMenu)#菜单右键槽函数
         self.tableWidget.cellDoubleClicked.connect(self.onTableWidgetCellDoubleClicked)
         self.VBoxLayout = QVBoxLayout()
@@ -56,7 +54,7 @@ class ShowLog(QDialog):
             for cloumn in self.list_cloumn:
                 if cloumn == 'rowid':
                     continue
-                item  = QTableWidgetItem((i[cloumn]))
+                item  = QTableWidgetItem(i[cloumn])
                 self.tableWidget.setItem(self.row, self.row2, item)
                 item.setTextAlignment(Qt.AlignHCenter|Qt.AlignVCenter)
                 self.row2 = self.row2 +1
@@ -88,31 +86,7 @@ class ShowLog(QDialog):
         row = item.row()
         action = pop_menu.exec_(self.tableWidget.mapToGlobal(pos))#显示菜单列表，pos为菜单栏坐标位置
         if action == delete_event:
-            r = QMessageBox.warning(self, "注意", "删除可不能恢复了哦！", QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
-            if r == QMessageBox.No:
-                return
-           
-            log_table = self.table + "_log_time"
-            database.c.execute("delete from {0} where rowid  = {1}".format(
-                log_table,self.information[row]["rowid"])).fetchall()
-            if self.table == "student":
-                item = database.c.execute(
-                "SELECT  cout,id_number from {0} where id_number = '{1}'".format(
-                    self.table,str(self.information[row]["id_number"]))).fetchall()[0] # 取出返回所有数据，fetchall返回类型是[()]
-                if item["cout"] is not None:
-                    database.c.execute(
-            "UPDATE {0} SET cout = {1} WHERE id_number = {2}".format(
-                self.table,item["cout"]-1,item["id_number"]))
-                    
-            imag_path = "img_information/{0}/{1}/log/{2}.jpg".format(
-                self.table,str(self.information[row]["id_number"]),str(self.information[row]["log_time"]))
-            if os.path.isfile(imag_path):
-                os.remove(imag_path)
-           
-            self.tableWidget.removeRow(row) 
-            self.information.pop(row)#删除信息列表
-            
-            return
+            self.delete(row)
 
         if action == imageView_event:
             imag_path = "img_information/{0}/{1}/log/{2}.jpg".format(
@@ -120,4 +94,29 @@ class ShowLog(QDialog):
             show_imag = ShowImage(imag_path,Qt.WhiteSpaceMode)
             show_imag.exec_()
 
+    def delete(self,row):
+        r = QMessageBox.warning(self, "注意", "删除可不能恢复了哦！", QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        if r == QMessageBox.No:
+            return
+        
+        log_table = self.table + "_log_time"
+        database.c.execute("delete from {0} where rowid  = {1}".format(
+            log_table,self.information[row]["rowid"])).fetchall()
+        if self.table == "student":
+            item = database.c.execute(
+            "SELECT  cout,id_number from {0} where id_number = '{1}'".format(
+                self.table,str(self.information[row]["id_number"]))).fetchall()[0] # 取出返回所有数据，fetchall返回类型是[()]
+            if item["cout"] is not None:
+                database.c.execute(
+        "UPDATE {0} SET cout = {1} WHERE id_number = {2}".format(
+                self.table,item["cout"]-1,item["id_number"]))
+                
+        imag_path = "img_information/{0}/{1}/log/{2}.jpg".format(
+            self.table,str(self.information[row]["id_number"]),str(self.information[row]["log_time"]))
+        if os.path.isfile(imag_path):
+            os.remove(imag_path)
+        
+        self.tableWidget.removeRow(row) 
+        self.information.pop(row)#删除信息列表
+        return
 
