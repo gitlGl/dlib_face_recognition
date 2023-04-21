1061c6b59c2a3287881c7440f0413b0b31678a1b
gitlG <1637885653@qq.com>
2023-04-21 23:26:21 +0800

    添加model.py

---
+++ src/model.py
@@ -0,0 +1,55 @@
+from peewee import *
+
+database1 = SqliteDatabase('resources/company.db')
+from .Database import database
+
+
+class UnknownField(object):
+    def __init__(self, *_, **__): pass
+
+class BaseModel(Model):
+    class Meta:
+        database = database1
+
+class Admin(BaseModel):
+    id_number = CharField(primary_key=True)
+    password = CharField()
+    salt = CharField()
+    vector = BlobField(null=True)
+
+    class Meta:
+        table_name = 'admin'
+
+class AdminLogTime(BaseModel):
+    id_number = CharField()
+    log_time = DateTimeField()
+
+    class Meta:
+        table_name = 'admin_log_time'
+        primary_key = False
+
+class Student(BaseModel):
+    cout = IntegerField(null=True)
+    gender = CharField()
+    id_number = CharField(primary_key=True)
+    password = CharField()
+    salt = CharField()
+    user_name = CharField()
+    vector = BlobField(null=True)
+
+    class Meta:
+        table_name = 'student'
+
+class StudentLogTime(BaseModel):
+    gender = BooleanField(null=True)
+    id_number = CharField()
+    log_time = DateTimeField()
+
+    class Meta:
+        table_name = 'student_log_time'
+        primary_key = False
+
+item  = Admin.select().where(Admin.id_number == "123456")
+print(item)
+database1.c.execute(item)
+
