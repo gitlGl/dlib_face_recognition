fb60b80882b4bb0161896560c7c33d31a56d7257
gitlG <1637885653@qq.com>
2023-07-13 17:21:03 +0800

    optimize:改变命名提高可读性

--- src/ShowLog.py
+++ src/ShowLog.py
@@ -8,19 +8,19 @@ from .Paging import Page
 import os
 from PyQt5.QtGui import QIcon
 class ShowLog(QDialog):
-    def __init__(self,id_number,str_list_column,table, list_cloumn):
+    def __init__(self,id_number, QTableWidget_column_name,table_name, table_cloumn_name):
         super().__init__()
         self.id_number = id_number
         self.page_count = 30
-        self.table = table
+        self.table_name = table_name
         self.setWindowFlags(Qt.WindowMinMaxButtonsHint | Qt.WindowCloseButtonHint)
         self.setWindowTitle("学生日志")
         self.setWindowIcon(QIcon("resources/日志.svg"))
         self.resize(300,400)
         self.tableWidget = QTableWidget(self)
-        self.list_cloumn = list_cloumn
-        self.page = Page(self.table+"_log_time",self.list_cloumn,
-        page_count=self.page_count,id_number = self.id_number)
+        self.table_cloumn_name = table_cloumn_name
+        self.page = Page(self.table_name+"_log_time",table_cloumn_name,
+        page_count=self.page_count,id_number = id_number)
         if  not self.page.information:
             QMessageBox.critical(self, '警告', '不存在用户或记录')
             self.close()
@@ -38,9 +38,9 @@ class ShowLog(QDialog):
         self.VBoxLayout.addWidget(self.page)
         self.setLayout(self.VBoxLayout)
         self.resize(480, 600)
-        columncout = len(str_list_column)
+        columncout = len( QTableWidget_column_name)
         self.tableWidget.setColumnCount(columncout)#根据数据量确定列数
-        self.tableWidget.setHorizontalHeaderLabels(str_list_column)
+        self.tableWidget.setHorizontalHeaderLabels( QTableWidget_column_name)
         self.setInformation()
 
     
@@ -52,15 +52,13 @@ class ShowLog(QDialog):
             i["log_time"] = i["log_time"].strftime("%Y-%m-%d-%H-%M")#时间格式化
            
             self.tableWidget.insertRow(row)
-            
-            row2 = 0
-            for row2,cloumn in enumerate(self.list_cloumn[1:]):#第一列为id，列表中不显示，所以从第二列开始，且id删除时需要使用
+            for row2,cloumn in enumerate(self.table_cloumn_name[1:]):#第一列为id，列表中不显示，所以从第二列开始，且id删除时需要使用
                 item  = QTableWidgetItem(i[cloumn])
                 self.tableWidget.setItem(row, row2, item)
                 item.setTextAlignment(Qt.AlignHCenter|Qt.AlignVCenter)
                 
             
-            imag_path = "img_information/{0}/{1}/log/{2}.jpg".format(self.table,i["id_number"],i["log_time"])
+            imag_path = "img_information/{0}/{1}/log/{2}.jpg".format(self.table_name,i["id_number"],i["log_time"])
             img_item =  QTableWidgetItem()
             img_item.setIcon(QIcon(imag_path))
             self.tableWidget.setItem(row, row2+1,img_item)
@@ -70,7 +68,7 @@ class ShowLog(QDialog):
            
     def onTableWidgetCellDoubleClicked(self, row, column):#双击槽函数 self.tableWidget.cellDoubleClicked.connect()
         imag_path = "img_information/{0}/{1}/log/{2}.jpg".format(
-            self.table,str(self.information[row]["id_number"]),str(self.information[row]["log_time"]))
+            self.table_name,str(self.information[row]["id_number"]),str(self.information[row]["log_time"]))
         show_imag = ShowImage(imag_path,Qt.WhiteSpaceMode)
         show_imag.exec_()
 
@@ -90,7 +88,7 @@ class ShowLog(QDialog):
 
         if action == imageView_event:
             imag_path = "img_information/{0}/{1}/log/{2}.jpg".format(
-                self.table,str(self.information[row]["id_number"]),str(self.information[row]["log_time"]))
+                self.table_name,str(self.information[row]["id_number"]),str(self.information[row]["log_time"]))
             show_imag = ShowImage(imag_path,Qt.WhiteSpaceMode)
             show_imag.exec_()
 
@@ -99,22 +97,22 @@ class ShowLog(QDialog):
         if r == QMessageBox.No:
             return
         
-        log_table = self.table + "_log_time"
+        log_table = self.table_name + "_log_time"
         database.c.execute("delete from {0} where id  = {1}".format(
             log_table,self.information[row]["id"]))
-        if self.table == "student":
+        if self.table_name == "student":
             database.c.execute(
             "SELECT count,id_number from {0} where id_number = '{1}'".format(
-                self.table,str(self.information[row]["id_number"])))
+                self.table_name,str(self.information[row]["id_number"])))
             item = database.c.fetchall()[0] # 取出返回所有数据，fetchall返回类型是[()]
             if item["count"] is not None:
                 database.c.execute(
         "UPDATE {0} SET count = {1} WHERE id_number = {2}".format(
-                self.table,item["count"]-1,item["id_number"]))
+                self.table_name,item["count"]-1,item["id_number"]))
         database.conn.commit()
                 
         imag_path = "img_information/{0}/{1}/log/{2}.jpg".format(
-            self.table,str(self.information[row]["id_number"]),str(self.information[row]["log_time"]))
+            self.table_name,str(self.information[row]["id_number"]),str(self.information[row]["log_time"]))
         if os.path.isfile(imag_path):
             os.remove(imag_path)
         
