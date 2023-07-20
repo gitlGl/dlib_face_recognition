4f49679d3b429dbf2eb951fe23c0140092035bb0
gitlG <1637885653@qq.com>
2023-07-20 16:40:30 +0800

    增加控件样式

--- src/GlobalVariable.py
+++ src/GlobalVariable.py
@@ -1,6 +1,7 @@
-import dlib
+import dlib,time,types
 from enum import Enum
-from .Database import Database
+from .Database import Database,type_database
+
 class models():
     def __init__(self):
         pass
@@ -21,7 +22,45 @@ class admin(Enum):
     password_max_length = 20
     password_min_length = 6
     
+def log_slow_query(query, execution_time):
+    with open('slow_queries.log', 'a') as f:
+        f.write(f'Slow query: {query}\nExecution time: {execution_time} seconds\n\n')
 
+def log_slow_queries(threshold):
+    def decorator(func):
+        def wrapper(self,query):
+            start_time = time.time()
+            result = func(self,query)
+            execution_time = time.time() - start_time
+            if execution_time > threshold:
+                log_slow_query(func.__name__, execution_time)
+            return result
+        return wrapper
+    return decorator
 
+@log_slow_queries(1.0) # Set threshold to 1 second
+def sqlite3_execute_query(self,query):
+    self.c.execute(query)
+    results = self.c.fetchall()
+    self.conn.commit()
+    return results
+
+def mysql_execute_query(self,query):
+    self.c.execute(query)
+    results = self.c.fetchall()
+    self.conn.commit()
+    return results
 database = Database()
+if type_database == "sqlite3" :
+    execute_query = sqlite3_execute_query
+    database.execute = types.MethodType(execute_query,database)
+elif type_database == "mysql" :
+    execute_query = mysql_execute_query
+    database.execute = types.MethodType(execute_query,database)
 database.creatble()
+
+# mysqld慢查询日志配置
+# slow_query_log = ON
+# long_query_time = 1
+# log_output = FILE
+# slow_query_log_file = C:\ProgramData\MySQL\MySQL Server X.X\data\slow_queries.log
